add3d3c247604297206cb951b4df6f81612cfc26
Firegamer4444 <diegopro07c@gmail.com>
2024-04-05 16:32:47 +0100

    Termine el programa en java

--- Practica 43.java
+++ Practica 43.java
@@ -1,33 +1,37 @@
 import java.util.Scanner;
 
 class main{
-    public void introducir_datos(){
+    public static void introducir_datos(){
         try{
         Scanner scanner = new Scanner(System.in);
-        System.out.print("introduce el nombre: ");
+        System.out.print("\nintroduce el nombre: ");
         String nombre = scanner.nextLine();
         System.out.print("introduce la edad: ");
         int edad = scanner.nextInt();
         if (edad >= 0){
             System.out.print("introduce la altura: ");
-            float altura = scanner.nextLine();
+            float altura = scanner.nextFloat();
+            System.out.println("Has introducido los datos correctamente");
+            System.out.println("nombre: " + nombre);
+            System.out.println("edad: " + edad + " años");
+            System.out.println("altura: " + altura + " metros");
         }
         else{
             System.out.print("Has introducido una edad negativa \nIntrodusca los datos de nuevo");
             introducir_datos();
         }
         } 
-        catch (NumberFormatException e){
+         catch (java.util.InputMismatchException e) {
             System.out.print("Has introducido algun dato mal \nIntrodusca los datos de nuevo");
             introducir_datos();
-        }
-        finally {
-        System.out.println("Has introducido los datos correctamente");
-        System.out.println("nombre: " + nombre);
-        }
+        }   
+        catch (java.util.NoSuchElementException e) {
+        System.out.println("\nHas interrumpido el programa");
+    }
+        
     }
 
     public static void main(String[] args) {
-
+        introducir_datos();
     }
 }
