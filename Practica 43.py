cbfe40fba03688538364be70e5fa4bab6a0ba7b0
Firegamer4444 <147237350+Firegamer4444@users.noreply.github.com>
2024-04-09 08:03:55 +0100

    Hice un pequqeño cambio al programa en python

--- Practica 43.py
+++ Practica 43.py
@@ -6,6 +6,10 @@ def introducir_datos():
         edad = int(input("introduce la edad: "))
         if edad >= 0 :
             altura = float(input("introduce la altura: "))
+            print("Has introducido los datos correctamente")
+            print("nombre: " , nombre)
+            print("edad: " , edad , "años")
+            print("altura: " , altura , "metros")
         else:
             print("Has introducido una edad negativa \nIntrodusca los datos de nuevo")
             introducir_datos()
@@ -14,11 +18,6 @@ def introducir_datos():
         introducir_datos()
     except KeyboardInterrupt:
         print("\nHas interrumpido el programa")
-    else:
-        print("Has introducido los datos correctamente")
-        print("nombre: " , nombre)
-        print("edad: " , edad , "años")
-        print("altura: " , altura , "metros")
         
 introducir_datos()
         
